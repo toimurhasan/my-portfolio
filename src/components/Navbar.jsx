9e35a30b85eec33238922742a1319c4eb1355254
toimurhasan <2imur.hasan@gmail.com>
2025-06-28 15:51:33 +0600

    navbar added with daisyui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,38 @@
 import React from "react";
+import { NavLink } from "react-router";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between py-6">
-      <div className="flex shrink-0 items-center">
-        <h3 className="font-extralight">/toimurhasan</h3>
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <h2 className="text-xl font-light cursor-pointer">/toimurhasan</h2>
       </div>
-    </nav>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <NavLink>About</NavLink>
+          </li>
+          <li>
+            <NavLink>Skills</NavLink>
+          </li>
+          <li>
+            <NavLink>Education</NavLink>
+          </li>
+          <li>
+            <NavLink>Experience</NavLink>
+          </li>
+          <li>
+            <NavLink>Projects</NavLink>
+          </li>
+          <li>
+            <NavLink>Contact</NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Resume</a>
+      </div>
+    </div>
   );
 };
 
