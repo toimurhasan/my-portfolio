f74629f81c106e4f3d0dbf4bb041570d83159906
toimurhasan <2imur.hasan@gmail.com>
2025-06-28 15:04:52 +0600

    added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className="flex items-center justify-between py-6">
+      <div className="flex shrink-0 items-center">
+        <h3 className="font-extralight">/toimurhasan</h3>
+      </div>
+    </nav>
   );
 };
 
