391b9bfb995e06e9b025502041d1464767e1303e
toimurhasan <2imur.hasan@gmail.com>
2025-06-28 16:10:30 +0600

    navbar and hero section added

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -5,11 +5,11 @@ import { Outlet } from "react-router";
 
 const RootLayout = () => {
   return (
-    <div>
+    <>
       <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
-    </div>
+    </>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,36 +3,38 @@ import { NavLink } from "react-router";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
-      <div className="navbar-start">
-        <h2 className="text-xl font-light cursor-pointer">/toimurhasan</h2>
+    <nav className="max-w-7xl mx-auto sticky top-0">
+      <div className="absolute navbar bg-base-100 shadow-sm rounded-b-xl">
+        <div className="navbar-start">
+          <h2 className="text-xl font-light ml-2 cursor-pointer">/toimurhasan</h2>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <NavLink>About</NavLink>
+            </li>
+            <li>
+              <NavLink>Skills</NavLink>
+            </li>
+            <li>
+              <NavLink>Education</NavLink>
+            </li>
+            <li>
+              <NavLink>Experience</NavLink>
+            </li>
+            <li>
+              <NavLink>Projects</NavLink>
+            </li>
+            <li>
+              <NavLink>Contact</NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Resume</a>
+        </div>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <NavLink>About</NavLink>
-          </li>
-          <li>
-            <NavLink>Skills</NavLink>
-          </li>
-          <li>
-            <NavLink>Education</NavLink>
-          </li>
-          <li>
-            <NavLink>Experience</NavLink>
-          </li>
-          <li>
-            <NavLink>Projects</NavLink>
-          </li>
-          <li>
-            <NavLink>Contact</NavLink>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Resume</a>
-      </div>
-    </div>
+    </nav>
   );
 };
 
