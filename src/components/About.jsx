bd9c21baea50fae61e17e1cafe3a8dc9297e0bf3
toimurhasan <2imur.hasan@gmail.com>
2025-07-11 02:23:44 +0600

    about-me story added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,32 +2,27 @@ import React from "react";
 
 const About = () => {
   return (
-    <section className="mt-10 lg:mt-0 pb-10 flex items-center justify-between max-w-7xl mx-auto px-4 flex-col lg:flex-row">
-      <div id="about" className="flex-1 justify-center">
+    <section className="mt-10 lg:mt-0 pb-10 flex lg:gap-2 items-center justify-center max-w-7xl mx-auto px-4 flex-col lg:flex-row">
+      <div id="about" className=" justify-center">
         <img
           src="/about-me.jpeg"
           className="lg:max-w-lg sm:max-w-md rounded-lg shadow-2xl"
           alt="about image"
         />
       </div>
-      <div className="lg:flex-[1.3] flex-1 lg:pl-6">
-        <h3 className="text-center py-6 text-2xl font-light">
+      <div className=" lg:pl-6">
+        <h3 className="lg:text-start text-center py-6 text-2xl font-light">
           About <span className="text-primary">Me</span>
         </h3>
-        <div className="mx-auto space-y-3">
-          <p className="text-justify">
-            I'm a dedicated and versatile web developer with experience in creating efficient,
-            scalable, and visually appealing web applications. My skill set includes front-end
-            development using HTML, CSS, JavaScript, and frameworks like React, as well as back-end
-            development with Node.js, Express, and MongoDB.
-          </p>
-          <p className="text-justify">
-            I’m comfortable working across the full stack and thrive in collaborative environments
-            where I can contribute to clean, maintainable code and user-centric design. I enjoy
-            staying up-to-date with the latest web technologies and trends, and I'm always looking
-            for ways to enhance user experience and performance. Whether it's building something
-            from scratch or optimizing an existing product, I'm passionate about turning ideas into
-            reality on the web.
+        <div className="text-sm sm:text-base lg:text-start text-center mx-auto space-y-3">
+          <p>I’m an introvert, and I used to think that was a disadvantage.</p>
+          <p>Until I discovered coding—where I could be my true self.</p>
+          <p>Being authentic had numerous advantages.</p>
+          <p>I could build without noise, think deeply, and stay focused for hours.</p>
+          <p>Debugging felt like meditation. Designing felt like solving puzzles.</p>
+          <p>I once stayed up all night trying to center a div.</p>
+          <p>
+            I finally found a career that fits <em>me</em>.
           </p>
         </div>
       </div>
