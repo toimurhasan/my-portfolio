618e0b4ae99792d68a7e8d12427050f7881aee17
toimurhasan <2imur.hasan@gmail.com>
2025-08-07 22:50:03 +0600

    resturctured --tools section

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -11,6 +11,7 @@ import {
   FaFigma,
   FaGitAlt,
   FaGithub,
+  FaGitSquare,
   FaHtml5,
   FaLaravel,
   FaNodeJs,
@@ -52,10 +53,12 @@ import {
 } from "react-icons/si";
 import { TbBrandNextjs, TbBrandVercel } from "react-icons/tb";
 import { VscVscodeInsiders } from "react-icons/vsc";
+import { Tooltip } from "react-tooltip";
 
 const Tools = () => {
   return (
     <section id="tools" className="pb-10">
+      <Tooltip id="my-tooltip" />
       <h3 className="text-center py-6 text-2xl font-light">
         Tools <span className="text-primary">I Use</span>
       </h3>
@@ -63,71 +66,230 @@ const Tools = () => {
         className=" text-3xl sm:text-4xl opacity-90 sm:opacity-80 "
         gradient="true"
         gradientColor={"oklch(14% 0.005 285.823)"}
-        speed={1}
+        speed={3}
         gradientWidth={80}
+        // pauseOnHover={"false"}
       >
-        <div className="border mx-1 p-3 rounded-2xl">
+        <a
+          href="https://www.w3schools.com/html"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="HTML5"
+        >
           <FaHtml5 />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
+        </a>
+        <a
+          href="https://www.w3schools.com/css"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="CSS3"
+        >
           <FaCss3 />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
+        </a>
+        <a
+          href="https://getbootstrap.com/docs/"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="Bootstrap"
+        >
           <FaBootstrap />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
+        </a>
+        <a
+          href="https://sass-lang.com/documentation/"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="Sass"
+        >
           <FaSass />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
+        </a>
+        <a
+          href="https://tailwindcss.com/docs"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="Tailwind CSS"
+        >
           <SiTailwindcss />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
+        </a>
+        <a
+          href="https://daisyui.com/components/"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="DaisyUI"
+        >
           <SiDaisyui />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
+        </a>
+        <a
+          href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="JavaScript"
+        >
           <IoLogoJavascript />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
+        </a>
+        <a
+          href="https://reactjs.org/docs/getting-started.html"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="React"
+        >
           <FaReact />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
+        </a>
+        <a
+          href="https://reactrouter.com/en/main"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="React Router"
+        >
           <SiReactrouter />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
+        </a>
+        <a
+          href="https://axios-http.com/docs/intro"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="Axios"
+        >
           <SiAxios />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
+        </a>
+        <a
+          href="https://firebase.google.com/docs"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="Firebase"
+        >
           <SiFirebase />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
+        </a>
+        <a
+          href="https://nodejs.org/en/docs"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="Node.js"
+        >
           <FaNodeJs />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
+        </a>
+        <a
+          href="https://expressjs.com/"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="Express.js"
+        >
           <SiExpress />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
+        </a>
+        <a
+          href="https://jwt.io/introduction"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="JWT"
+        >
           <SiJsonwebtokens />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
+        </a>
+        <a
+          href="https://www.mongodb.com/docs/"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="MongoDB"
+        >
           <SiMongodb />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
-          <TbBrandNextjs />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
-          <BiLogoTypescript />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
-          <TbBrandVercel />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
-          <SiNetlify />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
+        </a>
+        <a
+          href="https://git-scm.com/doc"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="Git"
+        >
+          <FaGitSquare />
+        </a>
+        <a
+          href="https://docs.github.com/"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="GitHub"
+        >
           <FaGithub />
-        </div>
+        </a>
+        <a
+          href="https://code.visualstudio.com/docs"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="VS Code"
+        >
+          <VscVscodeInsiders />
+        </a>
+        <a
+          href="https://docs.cpanel.net/"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="cPanel"
+        >
+          <FaCpanel />
+        </a>
+        <a
+          href="https://docs.npmjs.com/"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="NPM"
+        >
+          <FaNpm />
+        </a>
+        <a
+          href="https://help.figma.com/hc/en-us"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="Figma"
+        >
+          <FaFigma />
+        </a>
+        <a
+          href="https://wordpress.org/support/"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="WordPress"
+        >
+          <FaWordpress />
+        </a>
+        <a
+          href="https://woocommerce.com/documentation/"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="WooCommerce"
+        >
+          <SiWoo />
+        </a>
+        <a
+          href="https://elementor.com/help/"
+          target="_blank"
+          className="border block mx-1 p-3 rounded-2xl cursor-pointer"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="Elementor"
+        >
+          <FaElementor />
+        </a>
       </Marquee>
-      <Marquee
+
+      {/* ==========//=========== Second Marquee =========//========== */}
+      {/* <Marquee
         className=" text-3xl sm:text-4xl opacity-90 sm:opacity-80 mt-4"
         gradient="true"
         direction="right"
@@ -135,18 +297,9 @@ const Tools = () => {
         speed={1}
         gradientWidth={80}
       >
-        <div className="border mx-1 p-3 rounded-2xl">
-          <VscVscodeInsiders />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
-          <FaCpanel />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
-          <FaGitAlt />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
-          <FaNpm />
-        </div>
+       
+      
+       
         <div className="border mx-1 p-3 rounded-2xl">
           <SiPnpm />
         </div>
@@ -174,28 +327,16 @@ const Tools = () => {
         <div className="border mx-1 p-3 rounded-2xl">
           <SiMongoose />
         </div>
-        <div className="border mx-1 p-3 rounded-2xl">
-          <FaFigma />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
-          <FaWordpress />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
-          <SiWoo />
-        </div>
-        <div className="border mx-1 p-3 rounded-2xl">
-          <FaElementor />
-        </div>
+       
+       
         <div className="border mx-1 p-3 rounded-2xl">
           <SiNotion />
         </div>
-        <div className="border mx-1 p-3 rounded-2xl">
-          <SiObsidian />
-        </div>
+      
         <div className="border mx-1 p-3 rounded-2xl">
           <FaSlack />
         </div>
-      </Marquee>
+      </Marquee> */}
     </section>
   );
 };
