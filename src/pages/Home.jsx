ee2e66c08d308256f835ad95abb4e5acd8766f77
toimurhasan <2imur.hasan@gmail.com>
2025-10-11 13:10:01 +0600

    removed experience section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
       <About></About>
       <Tools></Tools>
       <Projects></Projects>
-      <Experience></Experience>
+      {/* <Experience></Experience> */}
       <Education></Education>
       <Contact></Contact>
     </div>
